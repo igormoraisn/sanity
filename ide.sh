10e4e2786722d202a30dac7def907703dbd7b143
Igor Nascimento <igorn428@gmail.com>
2015-06-10 09:59:01 -0300

    Create the ide.sh file

---
+++ ide.sh
@@ -0,0 +1,22 @@
+# Universidade Federal de Sergipe
+# Centro de Ciências Exatas e Tecnologias - CCET
+# Departamento de Computação - DCOMP
+# Sanity
+
+#!/bin/bash
+
+# Módulo para a detecção e testes acercas das IDE's:
+# Eclipse
+# Code::Blocks
+# Geany
+# Gedit
+# Netbeans
+# Mysql-Workbench ?
+# Codelite ?
+# Monodevelop ?
+# Bluefish ?
+# Anjuta ?
+# Emacs ?
+# ...
+
+echo "Opinem!"
