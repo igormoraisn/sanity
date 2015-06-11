d369e55c9fe3c782c70f841c325c04746c0aa3ef
Igor Nascimento <igorn428@gmail.com>
2015-06-11 11:59:44 -0300

    New features added

--- rede.sh
+++ rede.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Modulo para a detecção de conectividade
-rm .rede.txt
+test_dir=www.google.com
+rede=.rede.txt
+rm $rede
 
 #ifconfig eth0 >> rede.txt
-ping -c3 www.google.com
+ping -c3 $test_dir
 	if [ $? -eq 0 ]; then
-		echo "rede 1 OK" >> .rede.txt
+		echo "rede 1 OK" >> $rede
 	else
-		echo "rede 0 Não Funcionando" >> .rede.txt
+		echo "rede 0 Não Funcionando" >> $rede
 fi
 echo "Teste de rede realizado!"
