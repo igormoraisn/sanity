98b323536b7949614b17dab5f7e426ac7fe6949e
Igor Nascimento <igorn428@gmail.com>
2015-06-15 13:43:44 -0300

    Adding g++.cpp

---
+++ src/g++.cpp
@@ -0,0 +1,16 @@
+/*  Universidade Federal de Sergipe
+ *  Centro de Ciências Exatas e Tecnológicas
+ *  Departamento de Computação
+ *  Sanity
+ */
+
+ #include <cstdio>
+ #include <iostream>
+ #include <string>
+
+
+ int main(){
+
+
+
+ }
