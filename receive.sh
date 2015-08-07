71f424f04fe3d7e0a5417a76bbce426c72071bd8
Igor Nascimento <igorn428@gmail.com>
2015-08-07 12:35:47 -0300

    Updating receive.sh file(Testing)

--- receive.sh
+++ receive.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-while read linha
-do
-	partes=($linha)
-	hostname=${partes[0]}
-	ip=${partes[1]}
-	nc -vvn -c -w 2 $ip 1800 > $hostname.txt
-done < iplist.txt
+#while read linha
+#do
+ip=10.0.2.15
+hostname=manjaro	
+#partes=($linha)
+	#hostname=${partes[0]}
+	#ip=${partes[1]}
+nc -vvn -c -w 2 $ip 3000 > $hostname.txt
+#done < iplist.txt
 
