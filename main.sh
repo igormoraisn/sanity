43f4008d8b90bd20fe09b65a95a9ec18687298c1
Igor Nascimento <igorn428@gmail.com>
2015-08-13 16:50:56 -0300

    Updating main.sh

--- main.sh
+++ main.sh
@@ -7,6 +7,14 @@
 
 #!/bin/bash
 
+checa_logs() {
+	if [ -e ~/.sanity/.logs/$1 ]; then
+		rm ~/.sanity/.logs/$1
+	fi
+}
+
+
+
 if [ ! -d ~/.sanity ]; then
 	mkdir ~/.sanity
 fi
@@ -25,9 +33,24 @@ if [ -e $date ]; then
 	rm $date
 fi
 
+checa_logs .dev.txt
+checa_logs .dev-error.txt
+checa_logs .ide.txt
+checa_logs .ide-error.txt
+checa_logs .lib.txt
+checa_logs .lib-error.txt
+checa_logs .net.txt
+checa_logs .net-error.txt
+checa_logs .office.txt
+checa_logs .office-error.txt
+checa_logs .util.txt
+checa_logs .util-error.txt
+
 date >> ~/.sanity/.date.txt
 hostname=$(hostname)
 log=~/.sanity/$hostname.txt
+
+checa_logs $hostname.txt
 
 run-parts --regex .sh modules/network
 run-parts --regex .sh modules/development
